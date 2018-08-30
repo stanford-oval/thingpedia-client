9574d75329e2aaa24b245b6cf1b64ddca937ac04
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-08-29 22:02:33 -0700

    Add tests for the API layer

--- test/index.js
+++ test/index.js
@@ -9,10 +9,17 @@ process.env.TEST_MODE = '1';
 async function par(array) {
     await Promise.all(array.map((fn) => fn()));
 }
+async function seq(array) {
+    for (let fn of array)
+        await fn();
+}
 
-par([
-    require('./test_http_client'),
-    require('./test_v2_device'),
-    require('./test_generic_rest'),
-    require('./test_rss_device'),
+seq([
+    require('./test_api'),
+    () => par([
+        require('./test_http_client'),
+        require('./test_v2_device'),
+        require('./test_generic_rest'),
+        require('./test_rss_device'),
+    ])
 ]);
