2e9afe4926236043f18facb83019d361df5d5745
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-10-20 11:32:04 -0700

    Fix test classes
    The old, hand-written JSON files incorrectly used "is_required"
    rather than "required", and so new TT files were migrated incorrectly

--- test/index.js
+++ test/index.js
@@ -7,14 +7,16 @@ process.on('unhandledRejection', (up) => { throw up; });
 process.env.TEST_MODE = '1';
 
 async function seq(array) {
-    for (let fn of array)
-        await fn();
+    for (let fn of array) {
+        console.log(`Running ${fn}`);
+        await require(fn)();
+    }
 }
 
 seq([
-    require('./test_api'),
-    require('./test_http_client'),
-    require('./test_v2_device'),
-    require('./test_generic_rest'),
-    require('./test_rss_device'),
+    ('./test_api'),
+    ('./test_http_client'),
+    ('./test_v2_device'),
+    ('./test_generic_rest'),
+    ('./test_rss_device'),
 ]);
