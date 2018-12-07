6b2528980042bcf1c91a6faa4eae0f2f4f9b0d88
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-12-07 11:44:45 -0800

    Generic REST: support more argument types
    And attempt a best effort conversion to ThingTalk types

--- test/test_unit.js
+++ test/test_unit.js
@@ -10,6 +10,9 @@
 // See LICENSE for details
 "use strict";
 
+const TT = require('thingtalk');
+const assert = require('assert');
+
 const Utils = require('../lib/modules/utils');
 
 const FORMAT_STRING_TEST_CASES = [
@@ -54,8 +57,205 @@ function testFormatString() {
     });
 }
 
+const PARSE_GENERIC_RESPONSE_TEST_CASES = [
+    [{
+        json_key: 'articles',
+        args: [
+            {
+                name: 'title',
+                type: 'String',
+            },
+            {
+                name: 'link',
+                type: 'Entity(tt:url)',
+            },
+            {
+                name: 'picture_url',
+                type: 'Entity(tt:picture)',
+                json_key: 'thumbnail'
+            },
+        ]
+    }, {
+        articles: [
+            {
+                title: 'Some title',
+                link: 'https://example.com/1',
+                thumbnail: 'https://example.com/1.png',
+            },
+            {
+                title: 'Some other title',
+                link: 'https://example.com/2',
+                thumbnail: 'https://example.com/2.png',
+            }
+        ]
+    }, [
+        {
+            title: 'Some title',
+            link: new TT.Builtin.Entity('https://example.com/1', null),
+            picture_url: new TT.Builtin.Entity('https://example.com/1.png', null),
+        },
+        {
+            title: 'Some other title',
+            link: new TT.Builtin.Entity('https://example.com/2', null),
+            picture_url: new TT.Builtin.Entity('https://example.com/2.png', null),
+        }
+    ]],
+
+    [{
+        json_key: 'response.value',
+        args: [
+            {
+                name: 'key',
+                type: 'String',
+            },
+            {
+                name: 'object',
+                type: 'Entity(foo:object)',
+            },
+            {
+                name: 'measure1',
+                type: 'Measure(ms)',
+            },
+            {
+                name: 'measure2',
+                type: 'Measure(C)',
+            },
+            {
+                name: 'number1',
+                type: 'Number',
+            },
+            {
+                name: 'number2',
+                type: 'Number',
+            },
+            {
+                name: 'currency1',
+                type: 'Currency',
+            },
+            {
+                name: 'currency2',
+                type: 'Currency',
+            },
+            {
+                name: 'currency3',
+                type: 'Currency',
+            },
+            {
+                name: 'date1',
+                type: 'Date',
+            },
+            {
+                name: 'date2',
+                type: 'Date',
+            },
+            {
+                name: 'date3',
+                type: 'Date',
+            },
+        ]
+    }, {
+        response: {
+            value: {
+                key: 'some_key',
+                object: {
+                    value: '111111',
+                    display: 'Some object'
+                },
+                ignored: 'ignored',
+                measure1: 7,
+                measure2: '8',
+                number1: 9,
+                number2: '10.5',
+                currency1: 1000,
+                currency2: '1001',
+                currency3: { value: 1002, unit: 'eur' },
+
+                date1: '2018-01-01T00:00:00Z',
+                date2: '1 Jan 2018',
+                date3: 1544211576140,
+            }
+        }
+    }, [
+        {
+            key: 'some_key',
+            object: new TT.Builtin.Entity('111111', 'Some object'),
+            measure1: 7,
+            measure2: 8,
+            number1: 9,
+            number2: 10.5,
+            currency1: new TT.Builtin.Currency(1000, 'usd'),
+            currency2: new TT.Builtin.Currency(1001, 'usd'),
+            currency3: new TT.Builtin.Currency(1002, 'eur'),
+            date1: new Date(Date.UTC(2018, 0, 1, 0, 0, 0)),
+            date2: new Date(2018, 0, 1, 0, 0, 0),
+            date3: new Date(1544211576140)
+        },
+    ]],
+
+        [{
+        json_key: 'response.values.0',
+        args: [
+            {
+                name: 'key',
+                type: 'String',
+            },
+            {
+                name: 'hashtags',
+                type: 'Array(Entity(tt:hashtag))',
+            },
+            {
+                name: 'actors',
+                type: 'Array(String)',
+            },
+            {
+                name: 'objects',
+                type: 'Array(Entity(foo:object))',
+            }
+        ]
+    }, {
+        response: {
+            values: [
+                {
+                    key: 'some_key',
+                    hashtags: 'foo,bar',
+                    actors: 'Leonardo DiCaprio, Kate Winslet, Billy Zane, Kathy Bates',
+                    objects: [
+                        {
+                            value: '111111',
+                            display: 'Some object'
+                        },
+                        {
+                            value: '111112',
+                            display: 'Some other object'
+                        },
+                    ]
+                }
+            ]
+        }
+    }, [
+        {
+            key: 'some_key',
+            objects: [new TT.Builtin.Entity('111111', 'Some object'),
+                      new TT.Builtin.Entity('111112', 'Some other object')],
+            hashtags: [new TT.Builtin.Entity('foo', null), new TT.Builtin.Entity('bar', null)],
+            actors: ['Leonardo DiCaprio', 'Kate Winslet', 'Billy Zane', 'Kathy Bates'],
+        },
+    ]]
+];
+
+function testParseGenericResponse() {
+    PARSE_GENERIC_RESPONSE_TEST_CASES.forEach(([block, response, expected], i) => {
+        console.log('Test Case #' + (i+1));
+        const generated = Utils.parseGenericResponse(response, block);
+        assert.deepStrictEqual(generated, expected);
+    });
+}
+
 async function main() {
+    console.log('testFormatString');
     testFormatString();
+    console.log('testParseGenericResponse');
+    testParseGenericResponse();
 }
 module.exports = main;
 if (!module.parent)
